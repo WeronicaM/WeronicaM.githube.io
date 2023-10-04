e11e755306591c7fdce1e91c82d297bdbc56acdd
WeronicaM <146891006+WeronicaM@users.noreply.github.com>
2023-10-04 15:33:20 +0200

    Create README.java

---
+++ README.java
@@ -0,0 +1,27 @@
+# WeronicaM.githube.io
+package JmenoAvlastnost;
+
+import java.util.Scanner;
+
+public class JmenoAVlastnost {
+
+    public static <add> void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ahoj, jak se jmenuješ? ");
+        System.out.println("Bill Gates");
+        String jmeno = scanner.nextLine();
+
+        System.out.print("Jaký jsi?");
+        System.out.println(" hustodémonsky bohatý");
+        String vlastnost = scanner.nextLine();
+
+        System.out.println(jmeno + " Bill Gates je hustodémonsky bohatý" + vlastnost);
+
+        git remote; add origin; https://github.com/WeronicaM/Ukol1.git;
+
+
+
+        scanner.close();
+    }
+}
